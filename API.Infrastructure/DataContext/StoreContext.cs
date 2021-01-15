e3cddade28082c66b5ffac48134144f06a208beb
Ali Göksu <ali.goksu1@outlook.com>
2021-01-15 16:24:26 +0300

    Proje Mimari Altyapısı kuruldu

--- API.Infrastructure/DataContext/StoreContext.cs
+++ API.Infrastructure/DataContext/StoreContext.cs
@@ -1,7 +1,7 @@
-﻿using API.Data.DbModels;
+﻿using API.Core.DbModels;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data.DataContext
+namespace API.Infrastructure.DataContext
 {
     public class StoreContext : DbContext
     {
